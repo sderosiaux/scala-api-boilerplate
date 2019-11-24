38a9c772a297e39e90ae19d88d9eb36e7080a099
Stéphane Derosiaux <derste@gmail.com>
2019-11-24 21:26:00 +0100

    Add tests

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -2,10 +2,13 @@ package example
 
 import example.Generators.nonEmptyStrings
 import example.HttpHelpers.assertReqRes
-import example.route.ApiRoutes
+import example.route.{ ApiRoutes, ExternalApiRoutes }
+import example.ziomodules.QuoterService
 import org.http4s._
 import zio.interop.catz._
 import zio.test._
+import io.circe.literal._
+import zio.ZIO
 
 object HelloSpec
     extends DefaultRunnableSpec(
@@ -21,8 +24,13 @@ object HelloSpec
           checkM(nonEmptyStrings) { s =>
             assertReqRes(x, s"/bye/$s", s"Bye, $s!", Status.InternalServerError)
           }
+        },
+        testM("should joke") {
+          val x = new ExternalApiRoutes[QuoterService].route
+          val qs = new QuoterService {
+            override val quoterService: QuoterService.Service[Any] = () => ZIO.succeed("lol")
+          }
+          assertReqRes(x, "/joke", """{"joke":"lol"}""", Status.InternalServerError).provide(qs)
         }
       )
     )
-
-// test avec un TestQuoter
