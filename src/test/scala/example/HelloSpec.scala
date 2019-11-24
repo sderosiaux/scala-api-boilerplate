e2c68feb5ebef89b5e7d8be7d2715ce9e4d8ceeb
Stéphane Derosiaux <derste@gmail.com>
2019-11-24 20:13:27 +0100

    Test

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,14 +1,34 @@
 package example
 
-import zio.UIO
+import cats.effect.Sync
+import example.route.ApiRoutes
+import zio.{ RIO, Task, UIO, ZIO }
+import zio.interop.catz._
+import zio.interop.catz.core._
 import zio.test._
 import zio.test.Assertion._
+import org.http4s._
+
+object HttpHelpers {
+  def req[F[_]](path: String): Request[F] = Request[F](Method.GET, Uri.unsafeFromString(path))
+}
 
 object HelloSpec
     extends DefaultRunnableSpec(
-      suite("Hello")(test("should work") {
-        assert("hello", equalTo("hello"))
-      }, testM("should work") {
-        UIO.effectTotal(assert("hello", equalTo("hello")))
-      })
+      suite("Hello")(
+        testM("should work") {
+          import org.http4s.implicits._
+          val req: Request[Task] = HttpHelpers.req[Task]("/hello/john")
+          val x = new ApiRoutes[Any]().getRoutes.head.orNotFound
+          val res: RIO[Any, Response[Task]] = x.run(req)
+
+          res.flatMap { res =>
+            val ress = res.as[String]
+            assertM(ress, equalTo("Hello, john!"))
+          }
+        },
+        testM("should work") {
+          UIO.effectTotal(assert("hello", equalTo("hello")))
+        }
+      )
     )
